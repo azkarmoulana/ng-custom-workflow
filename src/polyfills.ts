da4884af21bc31e8fbf4cd2f2afef8e5207e6df2
azkarmoulana <azkar.moulana@gmail.com>
2020-03-02 00:31:54 +0530

    changed polyifll imports for core-js @3.0

---
+++ src/polyfills.ts
@@ -0,0 +1,7 @@
+// import 'core-js/es6';
+// import 'core-js/es7/reflect';
+import 'core-js/es/array';
+import 'core-js/es/reflect';
+import 'core-js/stable/reflect';
+import 'core-js/features/reflect'
+require ('zone.js/dist/zone');
